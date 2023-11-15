ea8ea6083043ccc429f689093317fd801002356a
Mo <moritz.schubert@sopat.de>
2023-11-15 19:02:26 +0100

    Add brython snake game see requirements install browser and javascript

--- demo_website/app/views.py
+++ demo_website/app/views.py
@@ -7,3 +7,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "home.html",{})
+
+def snake(request):
+    return render(request, "snake.html",{})
