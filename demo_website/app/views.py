924b54e6a825003164b3058c0feb530ddb7f4d81
Maximilian Lau <maximilian.lau7@gmail.com>
2023-12-12 15:41:41 +0100

    adds new content page - the checkin

--- demo_website/app/views.py
+++ demo_website/app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render, HttpResponse
+import random
 
-# # Create your views here.
-# def home(request):
-#   return HttpResponse("Hi Juri!")
+from .forms import CheckinForm
 
 # Create your views here.
 def home(request):
     return render(request, "home.html",{})
+
+def checkin(request):
+    if request.method == 'POST':
+        form = CheckinForm(request.POST)
+        if form.is_valid():
+            values = list(form.cleaned_data.values())
+            if False in values:
+                form = CheckinForm(initial=form.cleaned_data)
+                form.submit_msg=""  
+            else:
+                #not all of the SBD can run good simultaneously
+                random.seed()
+                i = random.choice(['squatcheckbox', 'benchcheckbox', 'deadliftcheckbox'])
+                form.cleaned_data[i] = False
+                form = CheckinForm(initial=form.cleaned_data)
+    else:
+        form = CheckinForm
+    return render(request, "checkin.html",{'form': form})
